18e906cc3be10230fbdeaa96d9bb7c87f49f9d35
impoopydoopy <113069987+impoopydoopy@users.noreply.github.com>
2022-09-08 01:48:46 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,23 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-
-        int[] array = {2, 7, 9, 8}; //массив с изначальными данными
-        boolean meetsCondition = true; //флаг совпадает ли массив с условием
-
-        for (int i = 0; i < array.length; i++) //вывод массива
+    public static void main(String[] args)
+    {
+        int min = 1, max = 101; //переменные мин и макс для определения границ цикла
+        for(int i = min; i < max; i++)
         {
-            System.out.println(array[i]);
+            if(i % 3 == 0 && i % 5 == 0) System.out.println("FizzBuzz"); //проверяем на кратность по отстатку от деления
+            else if(i % 3 == 0) System.out.println("Fizz");
+            else if(i % 5 == 0) System.out.println("Buzz");
+            else System.out.println(i); //если не выполняется ни одно условие просто выводим число
         }
-
-        for (int i = 1; i < array.length; i++) //проверка на совпадение с условием
-        {
-            if (array[i] < array[i - 1]) { //сравниваем все элементы
-                meetsCondition = false;
-                break;
-            }
-        }
-
-        if (meetsCondition) System.out.println("true"); //вывод результата
-        else System.out.println("false");
     }
 }
